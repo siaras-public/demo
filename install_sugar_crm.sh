564e6ccf11d11b6744ed63e2575c18cf0781cd42
mraj <mraj@siaras.com>
2015-04-17 18:08:43 -0700

    Picking up latest changes from demo

--- install_sugar_crm.sh
+++ install_sugar_crm.sh
@@ -2,9 +2,6 @@
 
 set -eux
 
-# Openup the Sugar CRM HTTP port
-iptables -A INPUT -i eth1 -p tcp -m tcp --dport 2080 -j ACCEPT
-
 sudo apt-get update
 sudo apt-get install -y wget git
 
@@ -23,6 +20,7 @@ if [ ! -d /root/docker-sugarcrm ]; then
     cd /root/docker-sugarcrm
     git checkout -b 1.0.0 1.0.0
 
+    cd /root/docker-sugarcrm
     cat<<EOF > docker-compose.yml
 sugarcrm:
   build: .
@@ -43,57 +41,13 @@ db:
 EOF
 
     cat<<EOF > Dockerfile
-FROM php:5.6-apache
+FROM jchiu/sugarcrm-demo
 
-ENV MAJOR_VERSION 6.5
-ENV MINOR_VERSION 20
-ENV SOURCEFORGE_MIRROR http://softlayer-dal.dl.sourceforge.net
 ENV WWW_FOLDER /var/www/html
-ENV PERF_TESTER_HOST $(hostname)
-
-RUN apt-get update && apt-get upgrade -y && \
-    apt-get install -y libcurl4-gnutls-dev libpng-dev unzip cron re2c php5-imap python
-
-RUN docker-php-ext-install mysql curl gd zip mbstring
-#       apt-get install -y php5-mysql php5-imap php5-curl php5-gd curl unzip cron
-
-WORKDIR /tmp
-
-RUN curl -O "\${SOURCEFORGE_MIRROR}/project/sugarcrm/1%20-%20SugarCRM%20\${MAJOR_VERSION}.X/SugarCommunityEdition-\${MAJOR_VERSION}.X/SugarCE-\${MAJOR_VERSION}.\${MINOR_VERSION}.zip" && \
-        unzip SugarCE-\${MAJOR_VERSION}.\${MINOR_VERSION}.zip && \
-        rm -rf \${WWW_FOLDER}/* && \
-        cp -R /tmp/SugarCE-Full-\${MAJOR_VERSION}.\${MINOR_VERSION}/* \${WWW_FOLDER}/ && \
-        chown -R www-data:www-data \${WWW_FOLDER}/* && \
-        chown -R www-data:www-data \${WWW_FOLDER}
-
+ENV PERF_TESTER_HOST PERF_TESTER_HOST_VAL
 RUN echo \${PERF_TESTER_HOST} > \${WWW_FOLDER}/hostname
 
-# RUN sed -i 's/^upload_max_filesize = 2M$/upload_max_filesize = 10M/' /usr/local/etc/php/php.ini
-
-ADD config_override.php.pyt /usr/local/src/config_override.php.pyt
-ADD envtemplate.py /usr/local/bin/envtemplate.py
-ADD init.sh /usr/local/bin/init.sh
-
-RUN chmod u+x /usr/local/bin/init.sh
-
-ADD crons.conf /root/crons.conf
-RUN crontab /root/crons.conf
-
-EXPOSE 80
-ENTRYPOINT ["/usr/local/bin/init.sh"]
 EOF
 
-    cat<<EOF > /etc/rc.local
+  cat<<EOF > /etc/rc.local
 #!/bin/sh
-
-(cd /root/docker-sugarcrm; \
-    /usr/local/bin/docker-compose up > /tmp/docker.log 2>&1)&
-EOF
-    chmod 755 /etc/rc.local
-
-    docker-compose build
-    /etc/rc.local
-fi
-
-
-
